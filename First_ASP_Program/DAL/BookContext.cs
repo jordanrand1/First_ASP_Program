6090a656dba24192c5b6d765752426b26fc2d68c
Jordan Rand <jorrand@gmail.com>
2019-01-08 16:14:36 -0700

    added a db connection

---
+++ First_ASP_Program/DAL/BookContext.cs
@@ -0,0 +1,30 @@
+﻿using First_ASP_Program.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace First_ASP_Program.DAL
+{
+    public class BookContext : DbContext
+    {
+        public BookContext() : base("BookContext")
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
